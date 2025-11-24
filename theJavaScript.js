e19764385bf55d041e60e18d3b27b9800a1585c8
Mondli <mondli46401@gmail.com>
2025-11-24 12:00:49 +0200

    Here I just finished the slider for the projects

--- theJavaScript.js
+++ theJavaScript.js
@@ -1,17 +1,27 @@
-const slider = document.querySelector(".slider");
-const slides = document.querySelectorAll(".project-box"); // Changed from .slide
+const swiper = new Swiper('.slider-container', {
+  loop: true,
 
-let index = 0;
-const slideWidth = 480; // width of each slide
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets: true
+  },
 
-document.getElementById("nextBtn").addEventListener("click", () => {
-    index++;
-    if (index >= slides.length) index = 0;
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
-});
-
-document.getElementById("prevBtn").addEventListener("click", () => {
-    index--;
-    if (index < 0) index = slides.length - 1;
-    slider.style.transform = `translateX(-${index * slideWidth}px)`;
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  breakpoints: {
+    0: {
+        slidesPerView: 1
+    },
+    768: {
+        slidesPerView: 2
+    },
+    1024: {
+        slidesPerView: 3
+    },
+  }
 });
